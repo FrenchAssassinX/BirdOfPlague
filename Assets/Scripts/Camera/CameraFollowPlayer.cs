9c51e5fb08671dc8088a49b6e0c46458626865db
Aurelien <thazet.aurelien@gmail.com>
2020-02-19 09:05:50 +0100

    Camera following player

---
+++ Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        this.transform.position = new Vector3(player.position.x, player.position.y, -10);
+    }
+}
--- Assets/Scripts/Player/MovePlayer.cs
+++ Assets/Scripts/Player/MovePlayer.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    public Rigidbody2D body2D;              // Body 2D of the sprite
-    float horizontalMove = 0f;              // Float to handling horizontal moves of the sprite
-    float verticalMove = 0f;                // Float to handling vertical moves of the sprite
-    public float moveSpeed = 10f;          // Move speed multiply by horizontalMoves or verticalMoves to create velocity 
+    public Rigidbody2D body2D;                  // Body 2D of the sprite
+    private float horizontalMove = 0f;          // Float to handling horizontal moves of the sprite
+    private float verticalMove = 0f;            // Float to handling vertical moves of the sprite
+    public float moveSpeed = 1.5f;              // Move speed multiply by horizontalMoves or verticalMoves to create velocity 
+
+    private bool bSpriteFacingRight = true;     // Boolean to flip sprite on the good direction
 
     void Start()
     {
@@ -18,6 +20,15 @@ public class MovePlayer : MonoBehaviour
     {
         horizontalMove = Input.GetAxis("Horizontal");       // Get x moves with inputs
         verticalMove = Input.GetAxis("Vertical");           // Get y moves with inputs
+
+        if (horizontalMove < 0 && bSpriteFacingRight)
+        {
+            FlipSprite();
+        }
+        else if (horizontalMove > 0 && !bSpriteFacingRight)
+        {
+            FlipSprite();
+        }
     }
 
     private void FixedUpdate()
@@ -28,5 +39,15 @@ public class MovePlayer : MonoBehaviour
         newVelocity.y = verticalMove * moveSpeed;           // Vertical move
 
         body2D.velocity = newVelocity;                      // Give to the body the new velocity
+    }
+
+    private void FlipSprite()
+    {
+        bSpriteFacingRight = !bSpriteFacingRight;           // Flip facing
+
+        /* Multiply by -1 the x player scale */
+        Vector3 scaling = transform.localScale;             // Get local scale of the transform
+        scaling.x *= -1;                                    // Invert direction
+        transform.localScale = scaling;                     // Affect new direction to the local scale of the transform
     }
 }

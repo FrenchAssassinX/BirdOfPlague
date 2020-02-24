839c6f3f0564b957f57e2364ddca1f920ac9671c
Aurelien <thazet.aurelien@gmail.com>
2020-02-24 17:54:18 +0100

    Not working: trying to create sight for knight

---
+++ Assets/Scripts/Monsters/Knight/CollidersKnight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollidersKnight : MonoBehaviour
+{
+    public BoxCollider2D colliderKnight;
+
+    void Start()
+    {
+        colliderKnight = this.gameObject.GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        /* Avoiding to rotate villager when colliding with rounded collider */
+        Quaternion newRotation = new Quaternion(0f, 0f, 0f, 0f);
+        this.transform.localRotation = newRotation;
+
+        /* Keep BoxCollider2D with the same position as the player */
+        colliderKnight.transform.position = this.transform.position;
+    }
+}
